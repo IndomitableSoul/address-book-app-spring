b636bb6803632831c94eba407ecd45978d0c6cf2
Nikita <amarnikita09@gmail.com>
2022-02-12 23:59:59 +0530

    Added: DTO and Model Layers to the App

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import lombok.ToString;
 
 @Data
-@ToString
 public class PersonDTO {
 
     public String fullName;
     public String email;
     public long contact;
 
+    public PersonDTO(String fullName, String email, long contact) {
+        this.fullName = fullName;
+        this.email = email;
+        this.contact = contact;
+    }
 }
---
+++ src/main/java/com/bridgelabz/addressbookapp/entity/PersonData.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.addressbookapp.entity;
+
+import com.bridgelabz.addressbookapp.dto.PersonDTO;
+import lombok.Data;
+
+@Data
+public class PersonData {
+
+    private long personId;
+    private String fullName;
+    private String email;
+    private long contact;
+
+    public PersonData(long personId, PersonDTO personDTO) {
+        this.personId = personId;
+        this.fullName = personDTO.fullName;
+        this.email = personDTO.email;
+        this.contact = personDTO.contact;
+    }
+}
