01777dfe70bc3dca00a0c8124aed440f5c58d5b2
Nikita <amarnikita09@gmail.com>
2022-02-13 01:11:26 +0530

    Refactored: added Lombok dependency into DTO and Entity Layers

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -5,7 +5,6 @@ import lombok.ToString;
 
 @Data
 public class PersonDTO {
-
     public String fullName;
     public String email;
     public long contact;
--- src/main/java/com/bridgelabz/addressbookapp/entity/PersonData.java
+++ src/main/java/com/bridgelabz/addressbookapp/entity/PersonData.java
@@ -5,7 +5,6 @@ import lombok.Data;
 
 @Data
 public class PersonData {
-
     private long personId;
     private String fullName;
     private String email;
