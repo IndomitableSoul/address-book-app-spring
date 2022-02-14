4da328f5da9cbd5b4583ebc25d90514822c9d990
Nikita <amarnikita09@gmail.com>
2022-02-15 00:01:46 +0530

    Added: remaining properties to Person DTO and Model

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -6,12 +6,15 @@ import lombok.ToString;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
 @Data
 public class PersonDTO {
 
     @Pattern(regexp = "[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Person name Invalid")
     public String fullName;
+
+    public String gender;
 
     @NotEmpty(message = "Email must not be empty")
     @Email(message = "Email Not Valid")
@@ -20,9 +23,22 @@ public class PersonDTO {
     @Pattern(regexp = "(91 )[1-9]{1}[0-9]{9}", message = "Enter valid mobile number")
     public String  contact;
 
-    public PersonDTO(String fullName, String email, String contact) {
+    public String address;
+    public String city;
+    public String state;
+    public long zip;
+    public List<String> type;
+
+    public PersonDTO(String fullName, String gender, String email, String contact,
+                     String address, String city, String state, long zip, List<String> type) {
         this.fullName = fullName;
+        this.gender = gender;
         this.email = email;
         this.contact = contact;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.type = type;
     }
 }
--- src/main/java/com/bridgelabz/addressbookapp/entity/PersonData.java
+++ src/main/java/com/bridgelabz/addressbookapp/entity/PersonData.java
@@ -3,17 +3,32 @@ package com.bridgelabz.addressbookapp.entity;
 import com.bridgelabz.addressbookapp.dto.PersonDTO;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PersonData {
     private long personId;
     private String fullName;
+    private String gender;
     private String email;
     private String contact;
+    private String address;
+    private String city;
+    private String state;
+    private long zip;
+    private List<String> types;
 
     public PersonData(long personId, PersonDTO personDTO) {
         this.personId = personId;
         this.fullName = personDTO.fullName;
+        this.gender = personDTO.gender;
         this.email = personDTO.email;
         this.contact = personDTO.contact;
+        this.address = personDTO.address;
+        this.city = personDTO.city;
+        this.state = personDTO.state;
+        this.zip = personDTO.zip;
+        this.types = personDTO.type;
+
     }
 }
