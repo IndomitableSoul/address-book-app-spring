c9dbcdf4db9091e860a0a9e8472030d27e9b286b
Nikita <amarnikita09@gmail.com>
2022-02-14 21:45:20 +0530

    Added: User Friendly Error Response in case validation fails

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -9,13 +9,14 @@ import javax.validation.constraints.Pattern;
 
 @Data
 public class PersonDTO {
-    @NotEmpty(message="Person name cannot be empty")
+
     @Pattern(regexp = "[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Person name Invalid")
     public String fullName;
-    @NotEmpty
-    @Email
+
+    @NotEmpty(message = "Email must not be empty")
+    @Email(message = "Email Not Valid")
     public String email;
-    @NotEmpty
+
     @Pattern(regexp = "(91 )[1-9]{1}[0-9]{9}", message = "Enter valid mobile number")
     public String  contact;
 
