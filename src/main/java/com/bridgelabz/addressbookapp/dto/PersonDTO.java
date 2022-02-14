ac0d414c64fd3b03546ad2e2034e4f16327ccae6
Nikita <amarnikita09@gmail.com>
2022-02-15 00:44:00 +0530

    Added: necessary validations to PersonsDTO

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -5,6 +5,7 @@ import lombok.ToString;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
@@ -14,6 +15,7 @@ public class PersonDTO {
     @Pattern(regexp = "[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Person name Invalid")
     public String fullName;
 
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
 
     @NotEmpty(message = "Email must not be empty")
@@ -23,10 +25,18 @@ public class PersonDTO {
     @Pattern(regexp = "(91 )[1-9]{1}[0-9]{9}", message = "Enter valid mobile number")
     public String  contact;
 
+    @Pattern(regexp = "^[0-9a-zA-Z,\\s -]{4,}$", message = "[Alphanumeric and , -] -> only allowed")
     public String address;
+
+    @Pattern(regexp = "^[a-zA-Z\\s]{4,}$", message = "Enter Valid City name")
     public String city;
+
+    @Pattern(regexp = "^[a-zA-Z\\s]{4,}$", message = "Invalid State name")
     public String state;
+    @Pattern(regexp = "^[1-9]{1}[0-9 \\s]{2,3}[0-9]{3}$", message = "Invalid Zip")
     public long zip;
+
+    @NotNull(message = "type should not be Empty")
     public List<String> type;
 
     public PersonDTO(String fullName, String gender, String email, String contact,
